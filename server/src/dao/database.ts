97b56dd6676717cacfe5f0708c1fa866d3277df0
romanchekashov <romanrich89@gmail.com>
2021-04-19 02:08:49 +0300

    update entity save date in UTC

--- server/src/dao/database.ts
+++ server/src/dao/database.ts
@@ -1,94 +1,95 @@
-import {DataTypes, Sequelize} from "sequelize";
-import {UserEntity, UserEntityInit} from "./models/UserEntity";
-import {CategoryEntity, CategoryEntityInit} from "./models/CategoryEntity";
-import {TestAnswerEntityInit} from "./models/test/TestItemAnswerEntity";
-import {TestItemEntityInit} from "./models/test/TestItemEntity";
-import {TestEntityInit} from "./models/test/TestEntity";
-import {TestCategoryEntityInit} from "./models/test/TestCategoryEntity";
-import {isProd, md5} from "../utils/utils";
+import { DataTypes, Sequelize } from "sequelize";
+import { UserEntity, UserEntityInit } from "./models/UserEntity";
+import { CategoryEntity, CategoryEntityInit } from "./models/CategoryEntity";
+import { TestAnswerEntityInit } from "./models/test/TestItemAnswerEntity";
+import { TestItemEntityInit } from "./models/test/TestItemEntity";
+import { TestEntityInit } from "./models/test/TestEntity";
+import { TestCategoryEntityInit } from "./models/test/TestCategoryEntity";
+import { isProd, md5 } from "../utils/utils";
+
+// https://sequelize.org/master/manual/dialect-specific-things.html
+require("pg").types.setTypeParser(1114, (stringValue: string) => {
+  return new Date(stringValue + "+0000");
+  // e.g., UTC offset. Use any offset that you would like.
+});
 
 let sequelize: Sequelize;
 
 const sequelizeDB = (): Sequelize => {
-    if (!sequelize) {
-        const dbName = `skilltest-${isProd() ? "prod" : "dev"}`;
+  if (!sequelize) {
+    const dbName = `skilltest-${isProd() ? "prod" : "dev"}`;
 
-        // setup a new database using database credentials set in .env
-        sequelize = new Sequelize(dbName,
-            process.env.DB_USER || "",
-            process.env.DB_PASSWORD || "",
-            {
-                host: "0.0.0.0",
-                dialect: "sqlite",
-                pool: {
-                    max: 5,
-                    min: 0,
-                    idle: 10000,
-                },
-                // Data is stored in the file `database.sqlite` in the folder `db`.
-                // Note that if you leave your app public, this database file will be copied if
-                // someone forks your app. So don't use it to store sensitive information.
-                storage: `./db/${dbName}.sqlite`,
-            });
+    // setup a new database using database credentials set in .env
+    sequelize = new Sequelize(
+      dbName,
+      process.env.DB_USER || "",
+      process.env.DB_PASSWORD || "",
+      {
+        dialect: "postgres",
+        dialectOptions: {
+          // Your pg options here
+        },
+      }
+    );
 
-        // authenticate with the database
-        sequelize
-            .authenticate()
-            .then(function (err) {
-                console.log("Connection established.");
-                // define new table: 'users'
-                UserEntityInit(sequelize);
-                TestAnswerEntityInit(sequelize);
-                TestItemEntityInit(sequelize);
-                CategoryEntityInit(sequelize);
-                TestEntityInit(sequelize);
-                TestCategoryEntityInit(sequelize);
+    // authenticate with the database
+    sequelize
+      .authenticate()
+      .then(function (err) {
+        console.log("Connection established.");
+        // define new table: 'users'
+        UserEntityInit(sequelize);
+        // TestAnswerEntityInit(sequelize);
+        // TestItemEntityInit(sequelize);
+        // CategoryEntityInit(sequelize);
+        // TestEntityInit(sequelize);
+        // TestCategoryEntityInit(sequelize);
 
-                setup();
-            })
-            .catch(function (err) {
-                console.log("Unable to connect to database: ", err);
-            });
+        setup();
+      })
+      .catch(function (err) {
+        console.log("Unable to connect to database: ", err);
+      });
 
-        // populate database with default users
-        // https://sequelize.org/master/manual/model-basics.html
-        function setup() {
-            console.log(`=====${process.env.NODE_ENV}=====`);
-            if (isProd()) {
-                // This creates the table if it doesn't exist (and does nothing if it already exists)
-                sequelize.sync();
-            } else {
-                // Using 'force: true' for demo purposes. It drops the table users if it already exists and then creates a new one.
-                sequelize.sync({force: true}).then(() => {
-                    // Add default users to the database
-                    UserEntity.create({
-                        username: "roman",
-                        password: md5(process.env.USER_ADMIN_PASSWORD || ""),
-                    }); // create a new entry in the users table
-
-                    ['web', 'programming', 'devops']
-                        .forEach(name => CategoryEntity.create({name}));
-                });
-            }
-        }
+    // populate database with default users
+    // https://sequelize.org/master/manual/model-basics.html
+    function setup() {
+      console.log(`=====${process.env.NODE_ENV}=====`);
+      if (isProd()) {
+        // This creates the table if it doesn't exist (and does nothing if it already exists)
+        sequelize.sync();
+      } else {
+        // Using 'force: true' for demo purposes. It drops the table users if it already exists and then creates a new one.
+        sequelize.sync({ force: false }).then(() => {
+          // Add default users to the database
+          //   UserEntity.create({
+          //     username: "roman",
+          //     password: md5(process.env.USER_ADMIN_PASSWORD || ""),
+          //   }); // create a new entry in the users table
+          //   ["web", "programming", "devops"].forEach((name) =>
+          //     CategoryEntity.create({ name })
+          //   );
+        });
+      }
     }
-    return sequelize;
+  }
+  return sequelize;
 };
 
 export const getBooleanAttribute = (defaultValue: boolean = false): any => ({
-    type: new DataTypes.INTEGER,
-    defaultValue: defaultValue ? 1 : 0,
-    validate: {
-        min: 0,
-        max: 1
-    },
-    allowNull: false,
+  type: new DataTypes.INTEGER(),
+  defaultValue: defaultValue ? 1 : 0,
+  validate: {
+    min: 0,
+    max: 1,
+  },
+  allowNull: false,
 });
 
 export const getIntPrimaryKey = (): any => ({
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+  type: DataTypes.INTEGER,
+  autoIncrement: true,
+  primaryKey: true,
 });
 
 export default sequelizeDB;
--- server/src/dao/models/UserEntity.ts
+++ server/src/dao/models/UserEntity.ts
@@ -1,30 +1,56 @@
-import {DataTypes, Model, Sequelize} from "sequelize";
-import {getIntPrimaryKey} from "../database";
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { getIntPrimaryKey } from "../database";
 
 export class UserEntity extends Model {
-    id!: number;
-    username!: string;
-    password!: string;
+  id!: number;
+  username!: string;
+  password!: string;
+  created!: Date;
+  updated!: Date;
 }
 
 export const UserEntityInit = (sequelize: Sequelize): void => {
-    UserEntity.init(
-        {
-            id: getIntPrimaryKey(),
-            username: {
-                type: new DataTypes.STRING(128),
-                unique: true,
-                allowNull: false,
-            },
-            password: {
-                type: new DataTypes.STRING(128),
-                allowNull: false,
-            },
-        },
-        {
-            tableName: "users",
-            timestamps: false,
-            sequelize, // passing the `sequelize` instance is required
-        }
-    );
+  UserEntity.init(
+    {
+      id: getIntPrimaryKey(),
+      username: {
+        type: new DataTypes.STRING(255),
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: new DataTypes.STRING(255),
+        allowNull: false,
+      },
+      // created: {
+      //   type: new DataTypes.DATE(),
+      //   allowNull: false,
+      //   get() {
+      //     const val = this.getDataValue("created");
+      //     console.log("+++++++++++++get", val, val.getTimezoneOffset());
+      //     return val;
+      //   },
+      //   set(val: Date) {
+      //     console.log("-------------set", val, val.getTimezoneOffset());
+      //     this.setDataValue(
+      //       "created",
+      //       new Date(
+      //         val.getTime() - (val.getTimezoneOffset() / 60) * 3600 * 1000
+      //       )
+      //     );
+      //   },
+      // },
+      // updated: {
+      //   type: new DataTypes.DATE(),
+      //   allowNull: false,
+      // },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: "updated",
+      sequelize, // passing the `sequelize` instance is required
+    }
+  );
 };
