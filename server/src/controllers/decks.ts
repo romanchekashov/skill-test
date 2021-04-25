1bbfefa15e6ae3c2f09a56b1bd9abb7d269b93b3
romanchekashov <romanrich89@gmail.com>
2021-04-25 15:09:45 +0300

    move deck logic to service

--- server/src/controllers/decks.ts
+++ server/src/controllers/decks.ts
@@ -1,15 +1,6 @@
 import { Request, Response, Router } from "express";
-import { DeckDto } from "@skill-test/data/dto/learn/DeckDto";
-import {
-  ASSOCIATION_ALIAS_DECK_TO_CARD,
-  ASSOCIATION_ALIAS_DECK_TO_USER,
-  DeckEntity,
-} from "../dao/models/learn/DeckEntity";
-import { mapDeckDtoToEntity, mapEntityToDtoDeck } from "../utils/converter";
-import { errorHandler } from "../utils/error_handler";
-import { UserEntity } from "../dao/models/UserEntity";
-import { CardEntity } from "../dao/models/learn/CardEntity";
 import deckService from "../services/deckService";
+import { errorHandler } from "../utils/error_handler";
 
 const decksRouter = Router();
 
@@ -25,25 +16,9 @@ decksRouter.get("/", (req: Request, res: Response) => {
 });
 
 decksRouter.get("/:id", (req: Request, res: Response) => {
-  var deckId = req.params.id;
-  DeckEntity.findByPk(deckId, {
-    include: [
-      {
-        model: UserEntity,
-        as: ASSOCIATION_ALIAS_DECK_TO_USER,
-      },
-      {
-        model: CardEntity,
-        as: ASSOCIATION_ALIAS_DECK_TO_CARD,
-      },
-    ],
-  })
-    .then((deck) => {
-      if (!deck) throw new Error(`Deck ${deckId} not found`);
-
-      const dto = mapEntityToDtoDeck(deck);
-      dto.cards.sort((a, b) => (a.id || 0) - (b.id || 0));
-      // console.log(deck, dto);
+  deckService
+    .getById(parseInt(req.params.id))
+    .then((dto) => {
       res.send(dto);
     })
     .catch((reason) => {
@@ -52,10 +27,10 @@ decksRouter.get("/:id", (req: Request, res: Response) => {
 });
 
 decksRouter.post("/", (req: Request, res: Response) => {
-  const dto: DeckDto = req.body;
-  DeckEntity.create(mapDeckDtoToEntity(dto, req.user.id))
-    .then((deck) => {
-      res.json(mapEntityToDtoDeck(deck));
+  deckService
+    .create(req.body, req.user.id)
+    .then((dto) => {
+      res.send(dto);
     })
     .catch((reason) => {
       errorHandler(reason, res);
--- server/src/services/deckService.ts
+++ server/src/services/deckService.ts
@@ -1,9 +1,47 @@
-import { DeckEntity } from "../dao/models/learn/DeckEntity";
-import { mapEntityToDtoDeck } from "../utils/converter";
+import { DeckDto } from "@skill-test/data/dto/learn/DeckDto";
+import { CardEntity } from "../dao/models/learn/CardEntity";
+import {
+  ASSOCIATION_ALIAS_DECK_TO_CARD,
+  ASSOCIATION_ALIAS_DECK_TO_USER,
+  DeckEntity,
+} from "../dao/models/learn/DeckEntity";
+import { UserEntity } from "../dao/models/UserEntity";
+import { mapDeckDtoToEntity, mapEntityToDtoDeck } from "../utils/converter";
 
-const getAll = () =>
-  DeckEntity.findAll().then((decks) => decks.map(mapEntityToDtoDeck));
+const getAll = (): Promise<DeckDto[]> => {
+  return DeckEntity.findAll().then((decks) => decks.map(mapEntityToDtoDeck));
+};
+
+const getById = (id: number): Promise<DeckDto> => {
+  return DeckEntity.findByPk(id, {
+    include: [
+      {
+        model: UserEntity,
+        as: ASSOCIATION_ALIAS_DECK_TO_USER,
+      },
+      {
+        model: CardEntity,
+        as: ASSOCIATION_ALIAS_DECK_TO_CARD,
+      },
+    ],
+  }).then((deck) => {
+    if (!deck) throw new Error(`Deck ${id} not found`);
+
+    const dto = mapEntityToDtoDeck(deck);
+    dto.cards.sort((a, b) => (a.id || 0) - (b.id || 0));
+    // console.log(deck, dto);
+    return dto;
+  });
+};
+
+const create = (dto: DeckDto, authorId: number): Promise<DeckDto> => {
+  return DeckEntity.create(mapDeckDtoToEntity(dto, authorId)).then(
+    mapEntityToDtoDeck
+  );
+};
 
 export default {
   getAll,
+  getById,
+  create,
 };
